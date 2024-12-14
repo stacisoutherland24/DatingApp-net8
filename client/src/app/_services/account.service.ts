55161fff13f38048e70b28b35c5f7f32df7a5a35
Staci Southerland <stacisoutherland24@gmail.com>
2024-12-14 09:49:53 -0600

    Add Angular service
    Add data binding to username/password and add service for http requests

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/';
+
+
+}
